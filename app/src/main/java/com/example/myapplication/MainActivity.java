15f518e46924848a93c2fa1505ce5aabeb499101
Polik1526 <trotpolik@gmail.com>
2023-08-29 13:50:06 +0300

    одано мої зміни

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,8 +7,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState1) {
+        super.onCreate(savedInstanceState1);
         setContentView(R.layout.activity_main);
     }
 }
